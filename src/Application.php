2990db59a58dbef7fd619800700cc59a5e67b823
Ondřej Hruboš <hruboson@gmail.com>
2021-04-22 13:19:26 +0200

    Reverted all changes to localhost version
    Trying another hosting with Cloudclusters.io

--- src/Application.php
+++ src/Application.php
@@ -68,11 +68,10 @@ class Application extends BaseApplication implements AuthenticationServiceProvid
         /*
          * Only try to load DebugKit in development mode
          * Debug Kit should not be installed on a production system
-         
+         */
         if (Configure::read('debug')) {
             $this->addPlugin('DebugKit');
         }
-        */
 
         // Load more plugins here
     }
